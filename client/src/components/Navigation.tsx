d866390438643ae1edd8014ba84448a387505ffa
manu-brighter <94370467+manu-brighter@users.noreply.github.com>
2026-03-22 16:04:42 -0400

    fix: Navigation.tsx ternary-Operator korrekt geschlossen (fehlende else-Klausel)

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -309,7 +309,7 @@ export function Navigation() {
                     </DropdownMenuItem>
                   </DropdownMenuContent>
                 </DropdownMenu>
-              )}
+              ) : null}
             </>
           )}
 
